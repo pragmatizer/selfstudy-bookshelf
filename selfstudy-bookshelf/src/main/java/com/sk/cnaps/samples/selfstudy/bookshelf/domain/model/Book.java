59b4cbacd360b8ad52d3eb0dbf71c1071c944b2f
pragmatizer <pragmatizer@gmail.com>
2018-04-09 13:29:06 +0900

    samples sub project 추가

---
+++ selfstudy-bookshelf/src/main/java/com/sk/cnaps/samples/selfstudy/bookshelf/domain/model/Book.java
@@ -0,0 +1,31 @@
+package com.sk.cnaps.samples.selfstudy.bookshelf.domain.model;
+
+import com.sk.cnaps.domain.model.AggregateRoot;
+
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+import lombok.ToString;
+import org.springframework.data.jpa.domain.AbstractPersistable;
+
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import java.util.Set;
+
+@Getter
+@Setter
+@ToString
+@NoArgsConstructor
+@Entity
+public class Book extends AbstractPersistable<Long> implements AggregateRoot {
+    @Column(nullable=false)
+    private String title;
+
+    private String subtitle;
+
+    private Set<Author> authors;
+}
+
+
+
+
